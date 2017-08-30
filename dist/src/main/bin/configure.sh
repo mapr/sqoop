8980815808bb76a98368efc11618724a7fa6042f
Sergey Svynarchuk <ssvinarchuk@cybervisiontech.com>
2017-08-30 14:55:34 +0300

    SQOOP-17: The ecosystem configure.sh should create a restart script sqoop2-<version>.restart.

--- dist/src/main/bin/configure.sh
+++ dist/src/main/bin/configure.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 MAPR_HOME="${BASEMAPR:-/opt/mapr}"
-MAPR_CONF_DIR="${MAPR_HOME}/conf/conf.d"
+MAPR_CONF_DIR="${MAPR_HOME}/conf"
+MAPR_CONF_WARDEN_DIR="${MAPR_CONF_DIR}/conf.d"
 DAEMON_CONF="$MAPR_HOME/conf/daemon.conf"
 SQOOP_VERSION="2.0.0"
 SQOOP_HOME="$MAPR_HOME"/sqoop/sqoop-"$SQOOP_VERSION"
@@ -9,6 +10,8 @@ SQOOP_CONF_DIR="${SQOOP_HOME}"/conf
 SQOOP_WARDEN_FILE="${SQOOP_HOME}"/conf.d/warden.sqoop2.conf
 SQOOP_CONF_FILE="${SQOOP_CONF_DIR}"/sqoop.properties
 secureCluster=0
+MAPR_USER=""
+MAPR_GROUP=""
 
 # isSecure is set in server/configure.sh
 if [ -n "$isSecure" ]; then
@@ -18,28 +21,18 @@ if [ -n "$isSecure" ]; then
 fi
 
 changeSqoopPermission() {
-
-  if [ -z "$MAPR_USER" ] ; then
-    MAPR_USER=$( awk -F = '$1 == "mapr.daemon.user" { print $2 }' "$DAEMON_CONF")
-  fi
-  if [ -z "$MAPR_GROUP" ] ; then
-    MAPR_GROUP=$( awk -F = '$1 == "mapr.daemon.group" { print $2 }' "$DAEMON_CONF")
-  fi
-
-  if [ -z "$MAPR_USER" ] ; then
-    MAPR_USER=mapr
-  fi
-  if [ -z "$MAPR_GROUP" ] ; then
-    MAPR_GROUP=mapr
-  fi
-
   if [ -f "$DAEMON_CONF" ]; then
-    if [ ! -z "$MAPR_USER" ]; then
-      chown -R "$MAPR_USER" "$MAPR_HOME/sqoop"
-    fi
-    if [ ! -z "$MAPR_GROUP" ]; then
-      chgrp -R "$MAPR_GROUP" "$MAPR_HOME/sqoop"
-    fi
+    MAPR_USER=$( awk -F = '$1 == "mapr.daemon.user" { print $2 }' "$DAEMON_CONF")
+    MAPR_GROUP=$( awk -F = '$1 == "mapr.daemon.group" { print $2 }' "$DAEMON_CONF")
+  else
+    MAPR_USER=`logname`
+    MAPR_GROUP="$MAPR_USER"
+  fi
+  if [ ! -z "$MAPR_USER" ]; then
+    chown -R "$MAPR_USER" "$MAPR_HOME/sqoop"
+  fi
+  if [ ! -z "$MAPR_GROUP" ]; then
+    chgrp -R "$MAPR_GROUP" "$MAPR_HOME/sqoop"
   fi
 }
 
@@ -47,14 +40,23 @@ changeSqoopPermission() {
 # Copying the warden service config file
 #
 setupWardenConfFile() {
-  if ! [ -d ${MAPR_CONF_DIR} ]; then
-    mkdir -p ${MAPR_CONF_DIR} > /dev/null 2>&1
+  if ! [ -d ${MAPR_CONF_WARDEN_DIR} ]; then
+    mkdir -p ${MAPR_CONF_WARDEN_DIR} > /dev/null 2>&1
   fi
 
   # Install warden file
-  if [ ! -e ${MAPR_CONF_DIR}/warden.sqoop2.conf ]; then
-    cp ${SQOOP_WARDEN_FILE} ${MAPR_CONF_DIR}
+  if [ ! -e ${MAPR_CONF_WARDEN_DIR}/warden.sqoop2.conf ]; then
+    cp ${SQOOP_WARDEN_FILE} ${MAPR_CONF_WARDEN_DIR}
   fi
+}
+
+createRestartFile(){
+  if ! [ -d ${MAPR_CONF_DIR}/restart ]; then
+    mkdir -p ${MAPR_CONF_DIR}/restart
+  fi
+
+  echo "maprcli node services -action restart -name sqoop2 -nodes $(hostname)" > "${MAPR_CONF_DIR}/restart/sqoop-$SQOOP_VERSION.restart"
+  chown -R $MAPR_USER:$MAPR_GROUP "${MAPR_CONF_DIR}/restart/sqoop-$SQOOP_VERSION.restart"
 }
 
 #
@@ -100,7 +102,7 @@ echo $secureCluster > ${SQOOP_CONF_DIR}/isSecure
 
 
 changeSqoopPermission
-
+createRestartFile
 setupWardenConfFile
 
 true
